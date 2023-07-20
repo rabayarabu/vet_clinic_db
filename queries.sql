45c5b3036b0adda43c68f5df5a0bdeebc5eec308
rabayarabu <rabu9149@gmail.com>
2023-07-20 17:01:01 +0600

    Update queries

--- queries.sql
+++ queries.sql
@@ -21,16 +21,19 @@ SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
  UPDATE animals SET species='pokemon' WHERE species IS NULL;
  SELECT * FROM animals;
 COMMIT;
+ SELECT * FROM animals;
 
 -- Delete Records
 BEGIN;
 DELETE FROM animals;
+SELECT * FROM animals;
 ROLLBACK;
 SELECT * FROM animals;
 BEGIN
 DELETE FROM animals WHERE date_of_birth>'2022-01-01';
 SAVEPOINT SP1;
 UPDATE animals SET weight_kg=weight_kg*-1;
+ROLLBACK;
 UPDATE animals SET weight_kg=weight_kg*-1 WHERE weight_kg<0;
 COMMIT;
 
